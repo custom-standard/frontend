573653b84110c7dde0f24872228c17d68408b025
rnignon <rnignon@naver.com>
2024-05-28 22:01:51 +0900

    feat: App, Index 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import { Outlet, Route, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import { Box, Flex } from "@chakra-ui/react";
 
 function App() {
-  return <div></div>;
+  const Layout = () => {
+    return (
+      <Box width="100%" maxWidth="1200px">
+        <Header />
+        <Outlet />
+        <Footer />
+      </Box>
+    );
+  };
+
+  return (
+    <Flex alignItems="center" flexDir="column">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route /> */}
+        </Route>
+      </Routes>
+    </Flex>
+  );
 }
 
 export default App;
