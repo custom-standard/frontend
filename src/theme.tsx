4e9169cd5b149ebec0437e9332967d8480c14ff4
rnignon <rnignon@naver.com>
2024-05-28 22:02:34 +0900

    feat: 대표 색상에 대한 테마 추가

---
+++ src/theme.tsx
@@ -0,0 +1,42 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    primary: {
+      50: "#FFFBEE",
+      100: "#FFF6DD",
+      200: "#FFD145",
+      300: "#FFCA28",
+      400: "#FFC512",
+      500: "#FFB900",
+      600: "#F6AD00",
+      700: "#EEA400",
+      800: "#E29C00",
+      900: "#D19000",
+    },
+    secondary: {
+      50: "#fffdf6",
+      100: "#fef8e0",
+      200: "#fef3c8",
+      300: "#feeaae",
+      400: "#fedf8f",
+      500: "#FFFCB3",
+      600: "#e1d097",
+      700: "#c3b67a",
+      800: "#a59a5d",
+      900: "#887d40",
+    },
+    tertiary: {
+      50: "#F5F3EE",
+      100: "#F0EDE5",
+      200: "#E9E5DB",
+      300: "#D8D1BF",
+      400: "#D1C9B4",
+      500: "#C9C0A9",
+      600: "#BFB59A",
+      700: "#A79E87",
+      800: "#8F8874",
+      900: "#777160",
+    },
+  },
+});
