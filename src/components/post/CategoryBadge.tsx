c0f7d890527ce47c7062dffe2ea33699036dd856
rnignon <rnignon@naver.com>
2024-06-11 23:01:24 +0900

    fix: 색상 초기 로딩 문제 해결

--- src/components/post/CategoryBadge.tsx
+++ src/components/post/CategoryBadge.tsx
@@ -6,22 +6,23 @@ interface Props {
   category: Category;
 }
 
+const colors = [
+  { bg: "bg-amber-100", text: "text-amber-400" },
+  { bg: "bg-orange-100", text: "text-orange-400" },
+  { bg: "bg-sky-100", text: "text-sky-400" },
+  { bg: "bg-rose-100", text: "text-rose-400" },
+  { bg: "bg-purple-100", text: "text-purple-400" },
+  { bg: "bg-emerald-100", text: "text-emerald-400" },
+  { bg: "bg-indigo-100", text: "text-indigo-400" },
+  { bg: "bg-neutral-100", text: "text-neutral-400" },
+];
+
 export default function CategoryBadge({ category }: Props) {
-  var color = [
-    "amber",
-    "orange",
-    "sky",
-    "rose",
-    "purple",
-    "emerald",
-    "indigo",
-    "neutral",
-  ];
+  const colorIndex = category.categoryId - 1;
+  const { bg, text } = colors[colorIndex] || {
+    bg: "bg-neutral-100",
+    text: "text-neutral-400",
+  };
 
-  var bgColor = `${color[category.categoryId - 1]}-100`;
-  var textColor = `${color[category.categoryId - 1]}-400`;
-
-  return (
-    <Badge className={`bg-${bgColor} text-${textColor}`}>{category.name}</Badge>
-  );
+  return <Badge className={`${bg} ${text}`}>{category.name}</Badge>;
 }
