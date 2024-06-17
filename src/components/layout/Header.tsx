bac4b5ce345b7eb546ff6345ad855f6a65eb018c
rnignon <rnignon@naver.com>
2024-06-17 22:46:03 +0900

    refactor: Header 디자인 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import CustomButton from "../button/CustomButton";
 import logo from "../../assets/image/logo.png";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -17,14 +16,14 @@ export default function Header() {
   };
 
   return (
-    <div className="flex top-0 w-full sticky drop-shadow px-10 py-4 bg-white justify-center">
+    <div className="flex top-0 w-full drop-shadow px-10 py-4 bg-white justify-center z-auto">
       <div className="flex w-full max-w-7xl justify-between items-center">
         <div className="flex items-center gap-10">
           <img
             src={logo}
             alt="logo"
             onClick={() => navigate("/")}
-            className="h-14 pointer-events-auto"
+            className="h-14 cursor-pointer"
           />
           <div className="flex gap-6">
             <Link to={"/"}>작품</Link>
@@ -33,7 +32,7 @@ export default function Header() {
           </div>
         </div>
         <div className="flex itmes-center gap-2">
-          <Profile size="sm" user={user} />
+          <Profile size="md" user={user} />
           <div className="flex items-center">
             <FaBell />
             <div className="relative right-3.5 top-0 rounded-md bg-red-500"></div>
