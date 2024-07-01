0980d12305973948e8a0a36588763e5ce19294f0
rnignon <rnignon@naver.com>
2024-07-01 23:29:23 +0900

    feat: Menu 버튼 기능 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "../../assets/image/logo.png";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Profile from "../user/Profile";
 import { FaBell } from "react-icons/fa";
+import { sampleUser } from "../../sampleData";
+import ProfileMenu from "../menu/ProfileMenu";
+import NotificationMenu from "../menu/NotificationMenu";
 
 export default function Header() {
   const navigate = useNavigate();
 
-  const user = {
-    uuid: "test UUID",
-    provider: "KAKAO",
-    email: "test@email.com",
-    name: "테스트",
+  const [openProfileMenu, setOpenProfileMenu] = useState(false);
+  const [openNotificationMenu, setOpenNotificationMenu] = useState(false);
+
+  const profileMenuRef = useRef<HTMLDivElement>(null);
+  const notificationMenuRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutSide = (e: MouseEvent) => {
+    if (
+      profileMenuRef.current &&
+      !profileMenuRef.current.contains(e.target as Node)
+    )
+      setOpenProfileMenu(false);
+    if (
+      notificationMenuRef.current &&
+      !notificationMenuRef.current.contains(e.target as Node)
+    ) {
+      setOpenNotificationMenu(false);
+    }
   };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutSide);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutSide);
+    };
+  }, []);
 
   return (
     <div className="flex top-0 w-full drop-shadow px-10 py-4 bg-white justify-center z-auto">
-      <div className="flex w-full max-w-7xl justify-between items-center">
+      <div className="flex w-full  justify-between items-center">
         <div className="flex items-center gap-10">
           <img
             src={logo}
@@ -32,10 +56,21 @@ export default function Header() {
           </div>
         </div>
         <div className="flex itmes-center gap-2">
-          <Profile size="md" user={user} />
-          <div className="flex items-center">
-            <FaBell />
-            <div className="relative right-3.5 top-0 rounded-md bg-red-500"></div>
+          <div ref={profileMenuRef}>
+            <div
+              onClick={() => setOpenProfileMenu(!openProfileMenu)}
+              className="hover:cursor-pointer"
+            >
+              <Profile size="md" user={sampleUser} link={false} />
+            </div>
+            {openProfileMenu && <ProfileMenu />}
+          </div>
+          <div ref={notificationMenuRef} className="m-auto">
+            <div onClick={() => setOpenNotificationMenu(!openNotificationMenu)}>
+              <FaBell className="hover:cursor-pointer" />
+              <div className="relative right-3.5 top-0 rounded-md bg-red-500"></div>
+            </div>
+            {openNotificationMenu && <NotificationMenu />}
           </div>
         </div>
       </div>
