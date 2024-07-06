52406e571db5ac18b342e8e5d4a556fa9ec954d0
rnignon <rnignon@naver.com>
2024-07-06 23:03:06 +0900

    refactor: 데이터를 sample data로 변경

--- src/components/cards/OrderCard.tsx
+++ src/components/cards/OrderCard.tsx
@@ -1,64 +1,13 @@
 import React from "react";
 import OrderInfo from "../order/OrderInfo";
-import OrderStatus from "../order/OrderStatus";
+import OrderStatusBar from "../order/OrderStatusBar";
 import ChatThumbnail from "../chat/ChatThumbnail";
+import { sampleChat, sampleOrder } from "../../sampleData";
 
 export default function OrderCard() {
   const fetchOrder = () => {};
 
   const fetchCurrentChat = () => {};
-
-  const category = {
-    categoryId: 1,
-    name: "식품",
-    description: "음식 관련 카테고리",
-  };
-
-  const date = {
-    date: new Date("2024-06-18T14:00:00+09:00"),
-    time: new Date("2024-06-18T14:00:00+09:00"),
-  };
-
-  const user = {
-    uuid: "test UUID",
-    provider: "KAKAO",
-    email: "test@email.com",
-    name: "테스트",
-    profileImageUrl:
-      "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F27738433597DCB1312",
-  };
-
-  const post = {
-    postId: 1,
-    category: category,
-    type: "구매",
-    title: "테스트 제목",
-    image: [],
-    dates: [date],
-    delivery: false,
-    minPrice: 30000,
-    maxPrice: 100000,
-    writer: user,
-  };
-
-  const order = {
-    orderId: 1,
-    post: post,
-    requester: user,
-    responder: user,
-    price: 10000,
-    date: date,
-    // TODO: 상태 업데이트
-    status: "wating",
-    isRequest: true,
-  };
-
-  const chat = {
-    other: user,
-    message: "hello",
-    time: new Date("2024-06-14T14:00:00+09:00"),
-    read: true,
-  };
 
   return (
     <div
@@ -66,14 +15,14 @@ export default function OrderCard() {
       className="flex justify-between gap-8 p-4 border border-black/15"
     >
       <div className="border border-black/10 p-4">
-        <OrderInfo order={order} />
+        <OrderInfo order={sampleOrder} />
       </div>
       <div className="flex flex-col flex-grow">
         <div className="px-6 pt-2 pb-6">
-          <OrderStatus level={2} />
+          <OrderStatusBar status={sampleOrder.status} />
         </div>
         <div className="px-4 py-2 border border-black/10">
-          <ChatThumbnail chat={chat} />
+          <ChatThumbnail chat={sampleChat} />
         </div>
       </div>
       <button className="h-8 text-sm text-white bg-red-700/50 px-2 py-1 rounded-[8px]">
