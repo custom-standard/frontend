b88414a4e561723438cfe98d31f87fcada2b0f27
rnignon <rnignon@naver.com>
2024-09-04 16:47:58 +0900

    feat: 날짜 포매터 함수 추가 및 수정

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,4 +1,23 @@
-export const toFormattedDate = (date: Date) => {
+export const toFormattedDate = (date: Date, time?: Date) => {
+  const now = new Date();
+  let result = "";
+
+  if (date.getFullYear() !== now.getFullYear()) {
+    result += `${date.getFullYear}년 `;
+  }
+  result += `${date.getMonth() + 1}월 ${date.getDate()}일 `;
+
+  if (time) {
+    result += `${date.getHours()}:${date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}`;
+  }
+
+  return result;
+};
+
+export const toFormattedDateAgo = (date: Date) => {
   const now = new Date();
   const givenDate = new Date(date);
 
