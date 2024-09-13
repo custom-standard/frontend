c61ae0de9b2ca76a4716d951ea834ce4ef7af81f
rnignon <rnignon@naver.com>
2024-09-13 19:57:21 +0900

    feat: API URL 파일 추가

---
+++ src/api/url.ts
@@ -0,0 +1,24 @@
+export const BASE_URL = process.env.DEV_API_URL;
+
+const POST_URL = "/post";
+const ORDER_URL = "/order";
+const PROPOSAL_URL = "/proposal";
+
+export const ENDPOINT = {
+  POST: POST_URL,
+  POST_DETAIL: (id: number) => `${POST_URL}/${id}`,
+  POST_ALL: `${POST_URL}/posts`,
+
+  ORDER: ORDER_URL,
+  ORDER_DETAIL: (id: number) => `${ORDER_URL}/${id}`,
+  ORDER_ALL: `${ORDER_URL}/orders`,
+  ORDER_COMFIRM: `${ORDER_URL}/confirm`,
+  ORDER_UPDATE_STATUS: `${ORDER_URL}/update/status`,
+  ORDER_UPDATE_DATA: `${ORDER_URL}/update/data`,
+
+  PROPOSAL: PROPOSAL_URL,
+  PROPOSAL_ALL: `${PROPOSAL_URL}/proposals`,
+  PROPOSAL_PROCESS: `${PROPOSAL_URL}/process`,
+
+  CATEGORY: `category/categories`,
+};
