8c0eaf455281a61968a7befb1830ceefcfcdf2c5
rnignon <rnignon@naver.com>
2024-10-23 19:17:49 +0900

    feat: 거래 카테고리 종류를 담는 categorySlice 구현

---
+++ src/store/category/categorySlice.ts
@@ -0,0 +1,14 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Category } from "../../types/Category";
+
+const initialState: Category[] = [];
+
+export const categorySlice = createSlice({
+  name: "category",
+  initialState,
+  reducers: {
+    setCategories: (_, action: PayloadAction<Category[]>) => {
+      return action.payload;
+    },
+  },
+});
